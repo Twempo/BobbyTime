cb206d7b179ab228a51ffbcb344acae7c573d409
OTHSCS061 <OTHSCS061@KATYISD.ORG>
2018-08-20 08:15:09 -0500

    Started PlayerController.cs

---
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+
+    }
+}
