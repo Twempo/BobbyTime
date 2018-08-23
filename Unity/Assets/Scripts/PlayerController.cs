52f43540752e30a7021417d83d53058640f14934
RattyBloom40 <e10benga@gmail.com>
2018-08-23 10:33:16 -0500

    Imported player model and started on camera-mouse rotation.

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float speed;
     public float jumpHeight;
+
+	public float mouseSensX;
+	public float mouseSensY;
+
 	float jumpMulti;
     Rigidbody rb;
 
     public GameObject cam;
+	public GameObject camTarget;
     Rigidbody camRb;
     Vector3 camOffset;
 
@@ -28,9 +33,12 @@ public class PlayerController : MonoBehaviour {
             rb.velocity = movement + new Vector3(0,jumpHeight*jumpMulti,0);
             jumpMulti -= Time.fixedDeltaTime * 2;
         }
-        Vector3 camNewPos = Vector3.Lerp(cam.transform.position, transform.position + camOffset, .2f);
+        Vector3 camNewPos = Vector3.Lerp(cam.transform.position, transform.position + camOffset, .6f);
+		//FIXME: camNewPos needs to rotate around player also.
         camRb.MovePosition(camNewPos);
-        Debug.Log(jumpMulti);
+		//camRb.transform.LookAt(camTarget.transform);
+		transform.RotateAround(transform.position,transform.up,Input.GetAxis("Mouse X")*mouseSensX);
+		camRb.transform.RotateAround( transform.position, transform.up, Input.GetAxis( "Mouse X" ) * mouseSensX );
     }
 
     void OnTriggerEnter(Collider c) {
